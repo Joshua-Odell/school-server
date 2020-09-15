427cc71ec6f9e1daf52c2bbf659e8af73eec4580
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-09-15 11:31:24 -0500

    fixed Database URL

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 const {CLIENT_ORIGIN} = require('./config');
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const IncidentService = require('./input/service')
-const inputRouter = require('./input/router')
-const outputRouter = require('./output/router')
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const IncidentService = require('./input/service');
+const inputRouter = require('./input/router');
+const outputRouter = require('./output/router');
 
 
-const app = express()
+const app = express();
 
-const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 const morganOption = ( NODE_ENV === 'production')
     ? 'tiny'
     : 'common' ;
 
-app.use(morgan(morganOption))
-app.use(express.json())
-app.use(helmet())
-app.use(cors({origin: CLIENT_ORIGIN}))
-app.use(inputRouter)
-app.use(outputRouter)
+app.use(morgan(morganOption));
+app.use(express.json());
+app.use(helmet());
+app.use(cors({origin: CLIENT_ORIGIN}));
+app.use(inputRouter);
+app.use(outputRouter);
 
 app.use(function errorHandler(error, req, res, next) {
     let response
@@ -36,6 +36,6 @@ app.use(function errorHandler(error, req, res, next) {
         response = { message: error.message, error }
     }
     res.status(500).json(response)
-})
+});
 
 module.exports = app
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/school',
+    DB_URL: process.env.DATABASE_URL,
     CLIENT_ORIGIN: 'http://localhost:3000/'
 }
